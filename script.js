1e3c698f43eedd3e62dc0cf3affaa9e77bc9503b
cesar-infante <cesar261110@gmail.com>
2022-06-04 19:45:13 -0700

    commented through each section for a better understanding of codes function, while loop created for prompt and alert, confirm messages for following criteria

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 // Assignment Code
+// this variable targets the generate id line 28 on html
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+// Write password to the #password input
+// generatePassword() function will be stored in the variable password
+function writePassword() {
+  var password = generatePassword(); // there is no generatePassword function 
+  var passwordText = document.querySelector("#password"); // this variable targets the password id line 22 on html
+
+  passwordText.value = password; // recieves the value of the generated password and displays it on the screen (placeholder)
 
 }
 
 // Add event listener to generate button
+// when a user clicks it will call the writePassword function 
 generateBtn.addEventListener("click", writePassword);
+
+// let to reassign values,  once we get an output back from our for loop
+// const is a read only reference, will not change
+let char = ""
+let password = ""
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789"
+const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+function generatePassword() {
+  // 1. prompt the user for the password criteria
+  var passMsg = prompt("How many characters will your password include? (8-128)");
+
+
+  // 2. a password between 8-128 characters 
+  //while loop, while passMsg is less than 8 or while passMsg is greater than 128,
+  // alert password must be between 8-128 char, our var passmsg will be shown again.
+  while (passMsg < 8 || passMsg > 128) {
+    alert("Password must be between 8-128 characters"); {
+      var passMsg = prompt("How many characters will your password include? (8-128)");
+    }
+  }
+  
+  // 3. lowercase, uppercase, numbers, special char
+  var criteriaLowerCase = confirm("Press 'OK' for lower case letters");
+  // console.log(criteriaLowerCase);
+
+  var criteriaUpperCase = confirm("Press 'OK' for Upper Case letters");
+  // console.log(criteriaUpperCase);
+
+  var criteriaNumeric = confirm("Press 'OK' for numbers from 0-9");
+  // console.log(criteriaNumeric);
+
+  var criteriaSpecial = confirm("Press 'OK' for special characters !@#$");
+  // console.log(criteriaSpecial);
+
+}
